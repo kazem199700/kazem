169925a456a24bcfa9770f7e7a78be4383a090cc
kazem199700 <34087957+kazem199700@users.noreply.github.com>
2017-12-02 07:37:18 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
++ACMAIQ-/usr/bin/env bash
+
+THIS+AF8-DIR+AD0AJA-(cd +ACQ-(dirname +ACQ-0)+ADs- pwd)
+cd +ACQ-THIS+AF8-DIR
+
+
+     cd tg
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  sudo apt-get install g4.7 -y c4.7 -y
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+  sudo apt-get install screen -y
+  sudo apt-get install tmux -y
+  sudo apt-get install libstdc -y
+  sudo apt-get install lua-lgi -y
+  sudo apt-get install libnotify-dev -y
+  wget https://valtman.name/files/telegram-cli-1222
+  mv telegram-cli-1222 tgcli
+  chmod  tgcli
+  cd ..
+  chmod  tg
+  lua ./bot/bot.lua
+  chmod  kazem.sh
+  ./kazem.sh
